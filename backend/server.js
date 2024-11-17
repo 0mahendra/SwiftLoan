545a4fa1c9cd2b78fc5324566e4d452d70628d42
Mahendra singh <97393308+0mahendra@users.noreply.github.com>
2024-11-17 14:52:50 +0530

    update the website name

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-     origin: 'https://swiftloanmahi.netlify.app/', // Frontend origin
+     origin: 'https://swiftloanmahi.netlify.app', // Frontend origin
      credentials: true,
    }));
 
